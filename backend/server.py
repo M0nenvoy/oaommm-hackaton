8dc31c17ac362c66e46c0b362db9c9816cd2ddc9
M0nenvoy <s.maluhin@astondevs.ru>
2024-11-06 22:29:32 +0400

    cors begone

--- backend/server.py
+++ backend/server.py
@@ -9,8 +9,18 @@ from fastapi import (
     HTTPException, UploadFile, FastAPI,
     WebSocket
 )
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 logger = logging.getLogger(__name__)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[""],
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=["*"],
+)
 
 api.setup()
 
