44aef512010dd7431914b26b396bdcf38920cfca
M0nenvoy <s.maluhin@astondevs.ru>
2024-11-06 22:52:52 +0400

    cors begone for sure

--- backend/server.py
+++ backend/server.py
@@ -16,9 +16,9 @@ logger = logging.getLogger(__name__)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=[""],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
